45be448af9c03c758421aea5eda6c862f6852cc3
Surel Sanap <surelsanap05@gmail.com>
2025-12-11 21:53:22 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
@@ -14,22 +13,3 @@ module.exports = {
     require("@tailwindcss/forms")
   ]
 };
-
-=======
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: ["class"],
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}"
-  ],
-  theme: {
-    extend: {}
-  },
-  plugins: [
-    require("@tailwindcss/forms")
-  ]
-};
-
->>>>>>> e26fc1da58dfa9fb599194d348340593e3ec9eb8
